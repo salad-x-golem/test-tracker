924daf469d4b3b4095dc52549a282e9f0744c7f8
scx1332 <sieciech@gmail.com>
2026-02-07 13:37:26 +0100

    Working on backend

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_PATH"],
+    url: process.env["DATABASE_PATH"] || "file:./dev.db",
   },
 });
---
+++ prisma/migrations/20260207123651/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "startedAt" DATETIME,
+    "finishedAt" DATETIME,
+    "parameters" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "uid" TEXT NOT NULL,
+    "originalName" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "testId" INTEGER NOT NULL,
+    CONSTRAINT "File_testId_fkey" FOREIGN KEY ("testId") REFERENCES "Test" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Test_name_key" ON "Test"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_uid_key" ON "File"("uid");
